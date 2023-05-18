991b64f4bce2df8f991351c83b29910ff16ea74f
Elena Levertova <elena@spacehaz.com>
2023-05-17 22:21:22 -0600

    Established the docker containers' communication. "Shells" processes an input data and returns a json string successfully.

--- Docker/resources/io.py
+++ Docker/resources/io.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import uuid
 
 import numpy as np
@@ -22,56 +23,65 @@ class IOList(MethodView):
         # convert string to dt list using list comprehension + split() + datetime.strptime()
         # from datetime import datetime
         # time_dt = [datetime.strptime(idx, '%Y-%m-%dT%H:%M:%S.%fZ') for idx in time_str]
-        time = list(io_data["time"].split(', '))
-        print('req times: ', time)
+        # time = list(io_data["time"].split(', '))
+        # print('req times: ', time)
 
         # energies = np.arange(200., 3000., 200.)
         # energies = "200., 400., 600., 800., 1000., 1200., 1400., 1600., 1800., 2000., 2200., 2400., 2600., 2800."
         # convert string to float list using list comprehension + split() + float()
         # energies = [float(idx) for idx in io_data["Energies"].split(', ')]
-        energies = io_data["Energies"]
-        print('energies: ', energies)
+        # energies = io_data["energies"]
+        # print('energies: ', energies)
 
-        Ls = [4., 4.]
-        print('Ls: ', Ls)
+        # Ls = [4., 4.]
+        # print('Ls: ', Ls)
 
-        Bmirrors = [np.floor(2.591e+04 * (L ** -2.98)) for L in Ls]
-        print('Bmirrors: ', Bmirrors)
+        # Bmirrors = [np.floor(2.591e+04 * (L ** -2.98)) for L in Ls]
+        # print('Bmirrors: ', Bmirrors)
 
         # User inputs time,X,Y,Z,pitch angles
         # Create the right json input structure
-        url = 'http:/localhost:23760'
-        data_input = {
+        url = "http://172.17.0.4:5000/api/magephem"
+        json_input = {
             # list of dates in YYYY-MM-DDTHH:MM:SS.mmm(uuu)Z format
-            # example: ["2020-12-31T12:59:49.0Z", "2020-12-31T12:59:59.0Z"]
-            'dates': time,
+            # example: ["2022-01-10T17:05:00.967250Z", "2022-01-10T18:18:00.967250Z"]
+            "dates": io_data["time"],
             # list of input 3-D coordinate sets (nested list)
-            # example: [[1,2,3],[4,5,6],[7,8,9]]
-            'X': io_data["xyz"],
+            # example: [[1,2,3],[4,5,6]]
+            "X": io_data["xyz"],
             # scalar input pitch angle
             # example: [10.0, 45.0, 90.0]
-            'alpha': io_data["pitch_angles"]
+            "alpha": io_data["pitch_angles"],
+            "kext": "opq",
+            "sys": "GDZ",
+            "outputs": ["I", "Bm", "L"]
         }
-        # Convert into json
-        json_input = json.dumps(data_input)
-        # Write Pretty-Print JSON data to file
-        with open("input.json", "w") as write_file:
-            json.dump(data_input, write_file, indent=4)
 
-        # # Change X,Y,Z to Ls, Bm using magephem and python requests
-        # # (time1,x1,y1,z1) -> (L1,Bm1)
-        # # (time2,x2,y2,z2) -> (L2,Bm2)
-        # # The output json structure will have L and Bm for each time
-        # output = requests.post(url, json=json_input)
-        #
-        # # parse output, the result is a Python dictionary
+        # Convert into json
+        # json_input = json.dumps(data_input)
+
+        print('input: ', json_input)
+        print('url: ', url)
+
+        # Change X,Y,Z to Ls, Bm using magephem and python requests
+        # (time1,x1,y1,z1) -> (L1,Bm1)
+        # (time2,x2,y2,z2) -> (L2,Bm2)
+        # The output json structure will have L and Bm for each time
+
+        try:
+            data_output = requests.post(url, json=json_input).json()
+            print('data_output: ', data_output)
+        except Exception as e:
+            print('e : ', e)
+
+        # parse output, the result is a Python dictionary
         # data_output = json.loads(output)
 
-        # output = pi.process_data(time, data_output["L"], data_output["Bm"], energies)
-        output = pi.process_data(time, Ls, Bmirrors, energies)
+        output = pi.process_data(io_data["time"], data_output["L"], data_output["Bm"], io_data["energies"])
+        #output = pi.process_data(time, Ls, Bmirrors, energies)
 
         # Pretty-Print JSON
-        json_output = {**io_data, "json_output": json.dumps(output)}
+        json_output = {**io_data, "json_output": output}
 
         # Write Pretty-Print JSON data to file
         with open("output.json", "w") as write_file:
--- Docker/schemas.py
+++ Docker/schemas.py
@@ -3,13 +3,11 @@ from marshmallow import Schema, fields
 
 class IOSchema(Schema):
     # inputs
-    time = fields.Str(required=True)  # dt format:  list of times
-    x = fields.Float(required=True)  # list of x corresponding to each time
-    y = fields.Float(required=True)  # list of y corresponding to each time
-    z = fields.Float(required=True)  # list of  z corresponding to each time
+    time = fields.List(fields.Str(), required=True)  # list of times (dt format)
     xyz = fields.List(fields.List(fields.Float()), required=True)  # list of  x,y,z corresponding to each time (floats)
-    Energies = fields.List(fields.Float(), required=True)  # list of energies (floats)
+    energies = fields.List(fields.Float(), required=True)  # list of energies (floats)
     pitch_angles = fields.List(fields.Float(), required=True)  # list of pitch angles (floats)
+
     # outputs
     # L = fields.Str(required=False)  # based on x,y,z
     # Bmirrors = fields.Str(required=False) # based on x,y,z
