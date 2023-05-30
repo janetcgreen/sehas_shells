79afba28794b1b1d58d204c5a362d2ac570dc696
Janet <jgreen@spacehaz.com>
2023-05-29 22:45:43 -0600

    Added base schema to order the inputs in the swagger ui

--- Docker/schemas.py
+++ Docker/schemas.py
@@ -1,11 +1,17 @@
 from marshmallow import Schema, fields
 
-class InSchema(Schema):
+
+class BaseSchema(Schema):
+    class Meta:
+        ordered = True
+
+class InSchema(BaseSchema):
     # Inputs to SHELLS app
     # There are two ways the app can be run
     # 1) with a list of requested times,locations for a set of energies and local PAs
     #    The locations will be changed to an L and each PA changed to Bm
     # 2) with a list of requested times and a list of energies,  Ls, and Bmirrors
+
     time = fields.List(fields.Str(), required=True,
                        description="list of dates and times with format 'YYYY-MM-DDTHH:MM:SS.fffuuuZ'",
                        example=["2022-01-01T00:00:00.000Z","2022-01-01T01:00:00.000Z"])  # list of times (dt format)
@@ -19,7 +25,7 @@ class InSchema(Schema):
                            description="1-D list of energies (keV) for the returned electron flux",
                            example=[500,600,700,1000])  # list of energies (floats)
     pitch_angles = fields.List(fields.Float(), required=True,
-                               description="1-D list of local pitch angles for the returned electron flux",
+                               description="1-D list of local pitch angles (degrees) for the returned electron flux",
                                example = [50,60,70])  # list of pitch angles (floats)
     # outputs
     # L = fields.Str(required=False)  # based on x,y,z
